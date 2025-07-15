2d964fa92ec8266cae7a157830922907f6f7226b
staronyinye <divinegift9999@gmail.com>
2025-07-15 10:43:19 +0100

    Add files via upload

---
+++ multiplication table.py
@@ -0,0 +1,23 @@
+# i = 1
+# while i <= 5:
+#     j = 1
+#     while j <= 12: 
+#         print(f"result:{i} X {j} = {i*j}")
+#         j += 1
+#     i += 1
+
+i = 1
+while i <= 10:
+        j = 1
+        while j <= 14:
+            print(f"{i}) X {j} = {i*j}")
+            j += 1
+        i += 1
+
+# i = 2
+# while i <= 5:
+#         j = 1
+#         while j <= 12:
+#             print(f"Result {i} * {j} = {i*j}")
+#             j += 1
+#         i += 1
